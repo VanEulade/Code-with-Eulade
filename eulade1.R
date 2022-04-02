c0913607d0836930b3d5680a23a4afab5eb1e354
VanEulade <92435606+VanEulade@users.noreply.github.com>
2022-04-02 09:27:40 +0300

    Update eulade1.R
    The Mann-Whitney U test with R

--- eulade1.R
+++ eulade1.R
@@ -13,3 +13,7 @@ shapiro.test(movieRating$Female)   # p < 0.05, data are not normal
 
 # Perform Mann-Whitney Test using "wilcox.test()" function
 wilcox.test(movieRating$Male, movieRating$Female, paired =  FALSE)
+data:  movieRating$Male and movieRating$Female
+W = 255.5, p-value = 0.1368
+alternative hypothesis: true location shift is not equal to 0
+Conclusion: We failed to reject null Hypothesis 
